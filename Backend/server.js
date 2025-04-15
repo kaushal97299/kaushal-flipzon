c3bff1b81ce2f67a075573f95e4c5fc95f5efa35
kaushalsharma97299@gmail.com <115640134+kaushal97299@users.noreply.github.com>
2025-04-16 00:19:34 +0530

    carousel image code update from backend

--- Backend/server.js
+++ Backend/server.js
@@ -15,8 +15,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 app.use("/ProfileImage", express.static("ProfileImage"));
-app.use("/uploadsCarou", express.static(path.join(__dirname, "uploadsCarou")));
-
+app.use("/uploadsCarou", express.static("uploadsCarou"));
 
 
 app.use("/api/auth", authRoutes);
