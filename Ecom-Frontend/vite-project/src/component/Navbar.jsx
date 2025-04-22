09c5a4e2b8c88043d78ff53c33939a1f058da394
kaushalsharma97299@gmail.com <115640134+kaushal97299@users.noreply.github.com>
2025-04-22 11:20:55 +0530

    loding

--- Ecom-Frontend/vite-project/src/component/Navbar.jsx
+++ Ecom-Frontend/vite-project/src/component/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar1 = () => {
           const timeoutId = setTimeout(() => {
             console.log("🔒 Token expired, logging out");
             handleLogout();
-          }, timeUntilExpiry * 1000);
+          }, (timeUntilExpiry-currentTime) * 1000);
           
 
           return () => clearTimeout(timeoutId);
