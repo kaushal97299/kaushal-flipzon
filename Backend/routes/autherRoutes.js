3edd0564f76e9a9d9452efda50b9f1b2cac41338
kaushalsharma97299@gmail.com <115640134+kaushal97299@users.noreply.github.com>
2025-08-04 15:17:31 +0530

    google authenticator1

--- Backend/routes/autherRoutes.js
+++ Backend/routes/autherRoutes.js
@@ -140,7 +140,6 @@ app.post("/google-signup", async (req, res) => {
     if (!user) {
       user = await User.create({ name, email, googleId, role: "user", gender: "other" });
     }
-
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
     const userData = user.toObject();
     delete userData.password;
