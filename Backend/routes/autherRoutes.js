78a8d007752187ca4587caff119e20da8f8c7b14
kaushalsharma97299@gmail.com <115640134+kaushal97299@users.noreply.github.com>
2026-01-25 11:45:48 +0530

    backend otp re

--- Backend/routes/autherRoutes.js
+++ Backend/routes/autherRoutes.js
@@ -27,7 +27,9 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
  // Store securely in environment variables
  const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
